333ffaf7b4299ede632ff3956eb244e38691b0dd
Shrey Sindher <ssindher11@gmail.com>
2023-02-20 02:24:45 +0530

    Implemented generate_password_page.dart

--- lib/res/color.dart
+++ lib/res/color.dart
@@ -7,4 +7,5 @@ const Color darkTextColor = Color(0xFF011821);
 const Color darkBlue = Color(0xFF171F46);
 const Color greyTextColor = Color(0xFF757784);
 const Color greyBorderColor = Color(0xFFCCCFD4);
+const Color greyTrackColor = Color(0xFFD9D9D9);
 const Color lightBg = Color(0xFFF0F4F7);
